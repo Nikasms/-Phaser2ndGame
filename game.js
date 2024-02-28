98cdb007437241f401a3f5a16ea0aa79af2951fd
Nikasms <148072883+Nikasms@users.noreply.github.com>
2024-02-28 15:43:26 +0200

    .

--- game.js
+++ game.js
@@ -63,24 +63,25 @@ function create() {
     //слідування камери за гравцем
    this.camera.maim.startFollow(player);
 // ходіння гравця в різні сторони
-   this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
+   
+this.anims.create({
+  key: 'left',
+  frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+  frameRate: 10,
+  repeat: -1
 });
 
 this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'dude', frame: 4 } ],
-    frameRate: 20
+  key: 'turn',
+  frames: [ { key: 'dude', frame: 4 } ],
+  frameRate: 20
 });
 
 this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
+  key: 'right',
+  frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+  frameRate: 10,
+  repeat: -1
 });
 }
 
